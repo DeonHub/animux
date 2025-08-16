9bc4fd3b7a4d56771fdaad6647ff3e83f7f23beb
DeonHub <impraimgideon89@gmail.com>
2025-08-16 02:17:50 +0000

    Fixed build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -39,14 +38,6 @@ android {
         }
     }
 }
-
-dependencies {
-
-  implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
-  implementation("com.google.firebase:firebase-analytics")
-
-}
-
 
 flutter {
     source = "../.."
